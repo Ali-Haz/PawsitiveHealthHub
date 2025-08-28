445ff811415c86e605c553e5f01212a776373cfd
ali-haz <ac159799@avcol.school.nz>
2025-08-28 14:52:38 +1200

    edited about page, edited footer, added privacy policy page

--- PawsitiveHealthHub/Controllers/HomeController.cs
+++ PawsitiveHealthHub/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace PawsitiveHealthHub.Controllers
             return View();
         }
 
+        public IActionResult AboutPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
